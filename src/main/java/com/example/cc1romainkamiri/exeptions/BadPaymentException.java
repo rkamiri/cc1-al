6bf5e25255cd840c41e91d07210f5fbcdde6f01d
rckam <rckmiri@gmail.com>
2021-12-28 18:34:50 +0100

    first commit

--- src/main/java/com/example/cc1romainkamiri/exeptions/BadPaymentException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cc1romainkamiri.exeptions;
-
-public class BadPaymentException extends Exception{
-    public BadPaymentException(){
-        super("Something went wrong with the payment ...");
-    }
-}
--- src/main/java/com/example/cc1romainkamiri/exeptions/NullUserException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cc1romainkamiri.exeptions;
-
-public class NullUserException extends Exception{
-    public NullUserException(){
-        super("User must not be null");
-    }
-}
